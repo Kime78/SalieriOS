3ede04cd9938f9ec48266b61e5b30ca9736eaac5
Kime78 <tmrhub824@gmail.com>
2021-01-25 09:01:51 +0200

    vmm working half

--- src/vmm.c
+++ src/vmm.c
@@ -24,7 +24,7 @@ bool vmm_setup_pages()
     }
 
     //map the first 32 MiB
-    for (size_t i = 0; i < 0x10000000; i += 0x1000) 
+    for (size_t i = 0; i < 0x100000000; i += 0x1000) 
     {
         vmm_map_page(kernel_map, higher_half(i), i, 0b11);
         //uintptr_t addr = i * 0x1000;
@@ -33,10 +33,10 @@ bool vmm_setup_pages()
         //vmm_map_page(kernel_map, addr, KERNEL_PHYS_OFFSET + addr, 0x03 | (1 << 8));
     }
     asm volatile(
-        "mov %%cr3,%0"
-        :
-        :"r"(kernel_map->pml4)
-        :"memory"
+        "mov %%cr3, %0"
+        : 
+        : "r" (kernel_map->pml4)
+        : "memory"
     );
 
     // char* rezultat;
@@ -68,30 +68,64 @@ bool vmm_create_pagemap(pagemap_t* map)
 
 uint64_t* virtual_mem;
 
-uint64_t* walk_to_page_and_map(uint64_t* current, uint16_t index)
+uintptr_t* walk_to_page_and_map(uint64_t* current, uint16_t index)
 {
-    if(current[index] == 0)
+    // if(current[index] == 0)
+    // {
+    //     current[index] = pmm_alloc(4096);
+    //     current[index] |= 0b11; 
+    // }
+
+    // return current[index] & ~(0b111111111111ULL);
+    uintptr_t ret;
+    if(current[index] & 0x1) //if its already mapped
     {
-        current[index] = pmm_alloc(4096);
-        current[index] |= 0b11; 
+        ret = current[index] & ~((uintptr_t)0xfff);
+    }
+    else 
+    {
+        ret = (uintptr_t)pmm_alloc(1);
+        if(ret == 0)
+        {
+            return NULL;
+        }
+
+        current[index] = ret | 0b111;
     }
 
-    return current[index] & ~(0b111111111111ULL);
+    return (void *)ret;
 }   
 
 bool vmm_map_page(pagemap_t *page_map, uintptr_t virt, uintptr_t phys, uintptr_t flags)
 {
     uint16_t offset = virt & 0b111111111111;    
-    uint16_t level1 = (virt >> 12);
-    uint16_t level2 = (virt >> 21);
-    uint16_t level3 = (virt >> 30);
-    uint16_t level4 = (virt >> 39);
+    // uint16_t level1 = (virt >> 12);
+    // uint16_t level2 = (virt >> 21);
+    // uint16_t level3 = (virt >> 30);
+    // uint16_t level4 = (virt >> 39);
+
+    uintptr_t level4 = (virt & ((uintptr_t)0x1ff << 39)) >> 39;
+    uintptr_t level3 = (virt & ((uintptr_t)0x1ff << 30)) >> 30;
+    uintptr_t level2 = (virt & ((uintptr_t)0x1ff << 21)) >> 21;
+    uintptr_t level1 = (virt & ((uintptr_t)0x1ff << 12)) >> 12;
 
     uint64_t* root = page_map->pml4;
     
     uint64_t* pml3 = walk_to_page_and_map(root, level4);
-    uint64_t* pml2 = walk_to_page_and_map(root, level3);
-    uint64_t* pml1 = walk_to_page_and_map(root, level2);
+    if(pml3 == NULL)
+    {
+        return false;
+    }
+    uint64_t* pml2 = walk_to_page_and_map(pml3, level3);
+    if(pml2 == NULL)
+    {
+        return false;
+    }
+    uint64_t* pml1 = walk_to_page_and_map(pml2, level2);
+    if(pml1 == NULL)
+    {
+        return false;
+    }
 
     pml1[level1] = phys | flags;
     
